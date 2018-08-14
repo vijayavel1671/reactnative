c645b5c079cfdf8aefa1944335ee435326f7edb6
vijay <vijayavel.r@aesasp.com>
2018-08-14 15:29:37 +0530

    Delete all the stuff

--- App/Common/Analytics.js
+++
@@ -1,8 +0,0 @@
-import {
-  GoogleAnalyticsTracker
-} from 'react-native-google-analytics-bridge';
-import Config from './Config';
-
-const gaTracker = new GoogleAnalyticsTracker(Config.Google.AnalyticsId)
-
-export default gaTracker;
